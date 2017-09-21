7a35a937d924e750dfcf2458980a5f1298fc62b5
jlinoff <jlinoff@versive.com>
2017-09-21 10:43:24 -0700

    Update to v0.5.0
    Modified the flow to be more failsafe. If an error occurs the
    the images are normally still present.
    
    Added the backup only capability (-b).
    
    Improved messaging.
    
    Added the -k option to force the images to always be kept when
    doing the resize operation. It combines the resize and backup
    operations into one.

--- docker-resize-img-mac.sh
+++ docker-resize-img-mac.sh
@@ -35,18 +35,31 @@ OPTIONS
                     the images:
                     ${B}\$(docker images --format '{{.Repository}}:{{.Tag}}')${R}
 
+    -b, --backup
+                    Create backup of the image in ${B}${BACKUP_DIR}${R}
+                    and exit. This is useful for capturing the state
+                    of the valid images.
+
+                    It creates an image.txt report, a restore.sh
+                    script and the image themselves.
+
+                    This operation does not affect docker.
+
     -f FILE, --file FILE
                     The docker image file.
                     You should never need to set this.
-                    Default: ${B}$DOCKER_IMG_FILE${R}
+                    Default: ${B}${DOCKER_IMG_REPO}${R}
 
     -h, --help      This help message.
 
-    -k, --keep      Keep the saved images.
+    -k, --keep      Keep the saved images in ${B}${BACKUP_DIR}${R}.
+
                     Normally the images are deleted but this option
-                    keeps them.
-                    Be careful, the images can consume a lot of
-                    disk space.
+                    keeps them so that you can recover if something
+                    goes wrong.
+
+                    This will keep you from losing data if anything
+                    goes wrong but it can consume a lot of disk space.
 
     -s SIZE, --size SIZE
                     The new image size. It can be larger or smaller
@@ -78,6 +91,9 @@ EXAMPLE USAGE
 
     # Example 6: Resize all images the new way.
     \$ ${B}$bn -s 120G -a${R}
+
+    # Example 7: Backup the images.
+    \$ ${B}$bn -b -a${R}
 
 LICENSE
     Copyright (c) 2017 by Joe Linoff
@@ -141,28 +157,34 @@ function dockerStart() {
 # Main
 # ================================================================
 # Grab command line arguments.
-readonly VERSION='0.4.1'
+readonly VERSION='0.5.0'
 readonly B=$(printf "\x1B[1m")
 readonly R=$(printf "\x1B[0m")
 readonly RB=$(printf "\x1B[1;31m")  # red-bold
 readonly RN=$(printf "\x1B[0;31m")  # red-normal
 readonly GB=$(printf "\x1B[1;32m")  # green-bold
 readonly GN=$(printf "\x1B[0;32m")  # green-normal
+readonly DATETIME_STAMP="$(date +%Y%m%d-%T%z | tr -d ':')"
+readonly BACKUP_DIR="backup/${DATETIME_STAMP}"
 
 SIZE=''
-DOCKER_IMG_FILE="$HOME/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/Docker.qcow2"
+DOCKER_IMG_REPO="$HOME/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/Docker.qcow2"
 IMAGES=()
 KEEP=0
+BACKUP=0
 
 while (( $# )) ; do
     arg=$1
     shift
     case "$arg" in
         -a|--all)
-            IMAGES=($(docker images --format '{{.Repository}}:{{.Tag}}'))
+            IMAGES=($(docker images --format '{{.Repository}}:{{.Tag}}' | grep -v '<none>'))
+            ;;
+        -b|--backup)
+            BACKUP=1
             ;;
         -f|--file)
-            DOCKER_IMG_FILE="$1"
+            DOCKER_IMG_REPO="$1"
             shift
             ;;
         -h|--help)
@@ -200,43 +222,117 @@ fi
 
 # Verify that the Docker file is present.
 # Note that I use $HOME instead of tilde to avoid expansion issues.
-DOCKER_IMG_FILE_EXISTS=1
-if [ ! -f $DOCKER_IMG_FILE ] ; then
+DOCKER_IMG_REPO_EXISTS=1
+if [ ! -f $DOCKER_IMG_REPO ] ; then
     echo -e "${RB}WARNING${RN}:${LINENO}: docker image file not found.${R}"
-    echo "         File: $DOCKER_IMG_FILE"
+    echo "         File: $DOCKER_IMG_REPO"
     echo "         No images can be restored."
     IMAGES=()
-    DOCKER_IMG_FILE_EXISTS=0
+    DOCKER_IMG_REPO_EXISTS=0
+fi
+
+# Save the images while docker is running.
+RESTORE=0
+if (( DOCKER_IMG_REPO_EXISTS )) ; then
+    # This can only be true of the image file exists.
+    n=${#IMAGES[@]}
+    echo "INFO:${LINENO}: $n images specified."
+    if (( n > 0 )) ; then
+        echo "INFO:${LINENO}: Creating $BACKUP_DIR."
+        RESTORE=1
+        mkdir -p $BACKUP_DIR
+        cd $BACKUP_DIR
+        docker images >images.txt
+
+        cat >restore.sh <<EOF
+#!/bin/bash
+# Automatically created by $0 to
+# restore backed up images.
+set -e
+EOF
+        chmod 0755 restore.sh
+
+        # Backup the specified images.
+        i=0
+        for IMG in ${IMAGES[@]} ; do
+            echo "INFO:${LINENO}: Backing up ${IMG}."
+            MATCHES=($(docker images --format '{{.Repository}}:{{.Tag}}' | grep "${IMG}"))
+            m=${#MATCHES[@]}
+            echo "INFO:${LINENO}:   Found $m matches."
+            for MATCH in ${MATCHES[@]} ; do
+                echo "INFO:${LINENO}:   Backing up image: ${B}$MATCH${R}."
+                (( i++ ))
+                REC=$(docker images --format '{{.Repository}}:{{.Tag}},{{.ID}},{{.Size}}' | \
+                         grep "${MATCH},")
+                ID=$(echo "$REC" | awk -F, '{print $2}')
+                echo "INFO:${LINENO}:     ID=${ID}"
+                IMG_SIZE=$(echo "$REC" | awk -F, '{print $3}')
+                echo "INFO:${LINENO}:     SIZE=${IMG_SIZE}"
+                TAR_FILE=$(printf "img%03d-%s.tar" "$i" "$ID")
+                echo "INFO:${LINENO}:     TAR_FILE=${TAR_FILE}"
+                cat >>restore.sh <<EOF
+
+# Restoring $MATCH.
+echo "INFO:\${LINENO}: Restoring $MATCH."
+docker load -i ${TAR_FILE}
+EOF
+                SAVE="time docker save ${MATCH} -o ${TAR_FILE}"
+                echo -e "INFO:${LINENO}:     ${GB}$SAVE${R}"
+                eval "$SAVE"
+                if (( $? )) ; then
+                    echo -e "${RB}ERROR${RN}:${LINENO}: Command failed.{R}"
+                    qemu-img info $DOCKER_IMG_REPO
+                    exit 1
+                fi
+            done
+        done
+    fi
+fi
+
+# Exit if this only a backup operation.
+if (( BACKUP )) ; then
+    echo "INFO:${LINENO}: Backup done."
+    exit 0
 fi
 
 # Get information about the image.
-if (( DOCKER_IMG_FILE_EXISTS )) ; then
+if (( DOCKER_IMG_REPO_EXISTS )) ; then
     # Check the image format.
-    IMG_FORMAT=$(qemu-img info $DOCKER_IMG_FILE 2>&1 | grep -i 'file format:' | awk '{print $3}')
+    echo "INFO:${LINENO}: Updating image: ${GB}${DOCKER_IMG_REPO}${R}."
+    IMG_FORMAT=$(qemu-img info $DOCKER_IMG_REPO 2>&1 | grep -i 'file format:' | awk '{print $3}')
     if ! [[ "$IMG_FORMAT" == "qcow2" ]] ; then
         echo -e "${RB}ERROR${RN}:${LINENO}: qemu-img format not qcow2.${R}"
-        qemu-img info $DOCKER_IMG_FILE
+        qemu-img info $DOCKER_IMG_REPO
         exit 1
     fi
-    echo "INFO:${LINENO}: Image format: $IMG_FORMAT."
+    echo "INFO:${LINENO}:   Image format: ${B}${IMG_FORMAT}${R}."
 
     # Get the image size.
-    IMG_SIZE=$(qemu-img info $DOCKER_IMG_FILE 2>&1 | grep -i 'virtual size:' | awk -F'(' '{print $2}' | awk '{print $1}')
-    printf "INFO:%d: Image virtual size: %'.f bytes.\n" $LINENO $IMG_SIZE
+    IMG_SIZE=$(qemu-img info $DOCKER_IMG_REPO 2>&1 | grep -i 'virtual size:' | awk -F'(' '{print $2}' | awk '{print $1}')
+    printf "INFO:%d:   Image virtual size: %'.f bytes.\n" $LINENO $IMG_SIZE
 
     # Set the image size.
     if [[ $SIZE = "" ]] ; then
         (( SIZE = 2 * IMG_SIZE ))
+        if (( SIZE > 999999999999 )) ; then
+            SIZE=$(echo "scale=2 ; 128849018880 / 2^40" | bc)T
+        elif (( SIZE > 999999999 )) ; then
+            SIZE=$(echo "scale=2 ; 128849018880 / 2^30" | bc)B
+        elif (( SIZE > 999999 )) ; then
+            SIZE=$(echo "scale=2 ; 128849018880 / 2^20" | bc)M
+        else
+            SIZE=$(echo "scale=2 ; 128849018880 / 2^10" | bc)k
+        fi
     fi
 
     # Report the new image size.
     if echo "$SIZE" | grep -q '^[0-9]*$' >/dev/null ; then
         # Only commaize if it is a number.
-        printf "INFO:%d: Updated image size: %'.f.\n" $LINENO $SIZE
+        printf "INFO:%d:   Updated image size: %'.f.\n" $LINENO $SIZE
     else
         # Could commaize here by splitting out the digits
         # portion but it isn't worth it.
-        printf "INFO:%d: Updated image size: %s.\n" $LINENO $SIZE
+        printf "INFO:%d:   Updated image size: %s.\n" $LINENO $SIZE
     fi
 
     # Make sure that docker is running.
@@ -255,54 +351,19 @@ if dockerRunning ; then
     fi
 fi
 
-# Save the images while docker is running.
-if (( DOCKER_IMG_FILE_EXISTS )) ; then
-    # This can only be true of the image file exists.
-    n=${#IMAGES[@]}
-    echo "INFO:${LINENO}: $n images specified."
-    if (( n > 0 )) ; then
-        # Backup the specified images.
-        i=0
-        for IMG in ${IMAGES[@]} ; do
-            (( i++ ))
-            FN=$(echo -n "$IMG" | base64)
-            echo -n "INFO:${LINENO}:    Saving image $i of $n: '$IMG' --> '$FN.tar' "
-
-            # Get the image size.
-            SZ=$(docker inspect $IMG | grep '"Size"' | awk -F: '{print $2}' | awk -F, '{print $1}' | awk '{print $1}')
-            if (( SZ > 999999999 )) ; then
-                SZSTR="$(echo "scale=2 ; $SZ / 1000000000" | bc)GB"
-            elif (( SZ > 999999 )) ; then
-                SZSTR="$(echo "scale=2 ; $SZ / 1000000" | bc)MB"
-            elif (( SZ > 999 )) ; then
-                SZSTR="$(echo "scale=2 ; $SZ / 1000" | bc)KB"
-            fi
-            echo "($SZSTR)."
-
-            # Now save the image.
-            rm -f ${FN}.tar
-            time docker save -o ${FN}.tar ${IMG}
-            if (( $? )) ; then
-                echo -e "${RB}ERROR${RN}:${LINENO}: image not found: '$IMG'.${R}"
-                exit 1
-            fi
-        done
-    fi
-fi
-
 # Quit docker.
 if dockerRunning ; then
     dockerStop
 fi
 
 # Resize the image.
-if [ -f $DOCKER_IMG_FILE ] ; then
+if [ -f $DOCKER_IMG_REPO ] ; then
     echo "INFO:${LINENO}: Removing the image file."
-    rm -f $DOCKER_IMG_FILE
+    rm -f $DOCKER_IMG_REPO
 fi
 
 echo "INFO:${LINENO}: Re-creating the image file."
-time qemu-img create -f qcow2 $DOCKER_IMG_FILE $SIZE
+time qemu-img create -f qcow2 $DOCKER_IMG_REPO $SIZE
 if (( $? )) ; then
     echo -e "${RB}ERROR${RN}:${LINENO}: Could not create the docker image file.${R}"
     exit 1
@@ -312,28 +373,12 @@ fi
 dockerStart
 
 # Restore the images.
-if (( ${#IMAGES[@]} > 0 )) ; then
-    # Re-populate the images.
+if (( RESTORE )) ; then
     echo "INFO:${LINENO}: Restore the docker images."
-    n=${#IMAGES[@]}
-    i=0
-    for IMG in ${IMAGES[@]} ; do
-        (( i++ ))
-        FN=$(echo -n "$IMG" | base64)
-        echo "INFO:${LINENO}:    Restoring image $i of $n: '$FN.tar' --> '$IMG'."
-        time docker load -q -i ${FN}.tar
-        if (( $? )) ; then
-            echo -e "${RB}WARNING${RN}:${LINENO}: Docker image not restored: '$FN.tar'.${R}"
-        fi
-    done
-
-    # Do not delete the images if the user specified -k.
+    ./restore.sh
     if (( ! KEEP )) ; then
-        echo "INFO:${LINENO}: Cleaning up."
-        for IMG in ${IMAGES[@]} ; do
-            FN=$(echo -n "$IMG" | base64)
-            rm -f $FN.tar
-        done
+        echo "INFO:${LINENO}: Deleting ${BACKUP_DIR}."
+        rm -rf ${BACKUP_DIR}
     fi
 fi
 
@@ -342,9 +387,9 @@ echo -e "INFO:${LINENO}: ${GB}Docker images.${R}"
 docker images
 
 echo -e "INFO:${LINENO}: ${GB}Docker image repo info.${R}"
-qemu-img info $DOCKER_IMG_FILE
+qemu-img info $DOCKER_IMG_REPO
 
 echo -e "INFO:${LINENO}: ${GB}Docker image repo size.${R}"
-ls -lh $DOCKER_IMG_FILE
+ls -lh $DOCKER_IMG_REPO
 
 echo -e "INFO:${LINENO}: ${GN}Done.${R}"
